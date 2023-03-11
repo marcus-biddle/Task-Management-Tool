083dbfa703aa85d22123f3a37672c5176e8c32dd
marcus-biddle <marcusbiddle16@gmail.com>
2023-03-10 16:11:12 -0800

    added more conditionals

--- client/src/helpers/conditionals/index.tsx
+++ client/src/helpers/conditionals/index.tsx
@@ -1,5 +1,7 @@
 export const showIf = (x: boolean) => (content: any) => (x ? content : null);
 
+export const showIfOrElse = (x: boolean) => (content: any) => (fallbackContent: any) => (x ? content : fallbackContent);
+
 // TODO format true validation
 export const getDate = (date?: string) => {
     const currentDay = new Date().toLocaleString('en-us', {  weekday: 'long' });
