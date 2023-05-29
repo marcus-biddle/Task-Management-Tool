723224b0101a929e02b867c363f29516d2e8479d
Mars <marcusbiddle16@gmail.com>
2023-05-29 13:28:49 -0700

    updated server model

--- server/models/server.model.js
+++ server/models/server.model.js
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-const serverSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+const serverSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, auto: true },
   title: { type: String, required: true },
   createdBy: {
     type: Schema.Types.ObjectId,
@@ -11,6 +11,6 @@ const serverSchema = new mongoose.Schema({
   active: { type: Boolean, default: true },
 });
 
-const Server = mongoose.model('Server', serverSchema);
+const Server = model('Server', serverSchema);
 
 export default Server;
